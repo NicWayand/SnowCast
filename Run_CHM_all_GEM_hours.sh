55fd06485239bcb034987dbfb31ad065b969df2b
NicWayand <nicway@gmail.com>
2016-09-15 09:35:58 -0600

    updates

--- Run_CHM_all_GEM_hours.sh
+++ Run_CHM_all_GEM_hours.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-MP_NUM_THREADS=4
+#MP_NUM_THREADS=4
 
 # Caller script to run CHM for all GEM hours
 
 # Run info
 cd /home/nwayand/snow_models/output_CHM/SnowCast
 
-./CHM -f GEM.json #-c config.option.startdate:$start_date 
+./CHM -f GEM_west_fortress.json #-c config.option.startdate:$start_date 
 
 # Convert output vtu files to tif files
-python /home/nwayand/snow_models/CHM/tools/vtu2geo/main.py vtu2geo_config.py
+#python /home/nwayand/snow_models/CHM/tools/vtu2geo/main.py vtu2geo_config.py
+echo Skipping vtu2geo, need to update!
 
 # Run ipython plotting scripts
-runipy Plot_CHM_output.ipynb Spatial_plots.ipynb
+#runipy Plot_CHM_output.ipynb Spatial_plots.ipynb
 runipy Plot_timeseries_at_CRHO_stations.ipynb Timeseries_plots.ipynb 
 
 # Push ipython notebook plots to github
