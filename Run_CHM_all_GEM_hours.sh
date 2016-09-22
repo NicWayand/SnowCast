bc5bdd197105e6bbba0ce1ef94a622b53cbd173a
NicWayand <nicway@gmail.com>
2016-09-22 12:13:22 -0600

    fixed paths

--- Run_CHM_all_GEM_hours.sh
+++ Run_CHM_all_GEM_hours.sh
@@ -14,7 +14,8 @@ echo Skipping vtu2geo, need to update!
 
 # Run ipython plotting scripts
 #runipy Plot_CHM_output.ipynb Spatial_plots.ipynb
-runipy Plot_timeseries_at_CRHO_stations.ipynb Timeseries_plots.ipynb 
+runipy ./plot_scripts/Plot_scatter_CHM_OBS.ipynb Scatter_plots.ipynb
+runipy ./plot_scripts/Plot_timeseries_at_CRHO_stations.ipynb Timeseries_plots.ipynb 
 
 # Push ipython notebook plots to github
 #http://askubuntu.com/questions/306176/hourly-git-push
