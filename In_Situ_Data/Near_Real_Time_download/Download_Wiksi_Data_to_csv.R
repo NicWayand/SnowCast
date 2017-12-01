db89b1bf62488afeee28466ff8f288d7f0fdbb1e
NicWayand <nicway@gmail.com>
2017-11-30 18:22:14 -0600

    working on crho download through wiski using Kevins R package

---
+++ In_Situ_Data/Near_Real_Time_download/Download_Wiksi_Data_to_csv.R
@@ -0,0 +1,48 @@
+library(WISKIr)
+
+X <- findWISKIstations('*')
+write.csv(X$station_name, 'wiski_stanames.csv')
+
+data_dir <- '/media/data2/SnowCast_station_data/CRHO_NRT/'
+
+# bad boys
+# "Level Forest" "PowerLine" Upper Forest Bow Hut
+
+stations <- c('Fortress Ridge','Vista View',"Bonsai Meteorological","Burstall Pass", "Centennial Ridge",
+    "Canadian Ridge", "Fortress Ledge",
+    "Fortress Ridge", "Fortress Ridge South Meteorological", "Fisera Ridge", "Helen",
+    "Hay Meadow", "Peyto Hut Main",
+    "Upper Clearing", "Vista View", "Canadian Ridge North")
+
+variables <- c('TEMPERATURE AIR','RelHum','SnowDepth','Snow Depth Quality Flag','WindDir','WindSpeed','AccumulatedPrecip',
+                'IncomingLWRad','IncomingSWRad','OutgoingLWRad','OutgoingSWRad','IntervalPrecip') #,'SurfTemp')
+
+for(i in 1:length(stations))
+{
+  print( paste0('Working on ... ',stations[i]) )
+  #sta_name = findWISKIstations(paste0('*',stations[i],'*'))
+  #print(sta_name)  
+  new_name <- stringr::str_replace_all(stations[i],' ','%20')
+  #print(new_name)
+  df<-findWISKItimeseries( new_name)
+#  print(df)
+  for (cvar in variables) {
+    if(cvar %in% df$parametertype_name) {
+        print(cvar)
+        id<-df[ df$parametertype_name == cvar & df$ts_name == '01.Original',]$ts_id
+  
+    if( length(id) > 1)
+    {
+        id <- id[1]
+     }
+    # Some times this *&^%*&%^(&  fails and not handeled right. So write a stupied R try catch
+    
+    ts0<-getWISKIvalues(id,timezone='MST',startDate = "2017-11-28", endDate = "2017-11-29")
+    
+    sane_var_name <- stringr::str_replace_all(cvar,' ','_')
+    sane_file_name <- paste0( stringr::str_replace_all(stations[i],' ','_'),'_',sane_var_name ,'.csv')
+    write.csv(ts0, paste0(data_dir,sane_file_name))
+  }
+}
+}
+
