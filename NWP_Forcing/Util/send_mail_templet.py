5a4c64bc838ea0de8006c7aa538e112fe457373f
NicWayand <nicway@gmail.com>
2018-01-18 11:16:48 -0600

    moved file

--- NWP_Forcing/Util/send_mail_templet.py
+++ NWP_Forcing/Util/send_mail_templet.py
@@ -3,6 +3,7 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
 # TO USE: Rename this file to 'send_mail.py' and update your personal settings below
+# Create symbolic links to the Download and GRIB2_to_NETCDF dirs (TODO: make a proper module so it can be imported)
 
 def send(body): 
     server = smtplib.SMTP('smtp.gmail.com', 587) # If using gmail
